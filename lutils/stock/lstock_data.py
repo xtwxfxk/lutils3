8bed6d7a51df519e376eaaff8a45498246dc9461
left5 <hua-qiang.zhang@doone.com.cn>
2020-08-24 15:48:21 +0800

    0824

--- lutils/stock/lstock_data.py
+++ lutils/stock/lstock_data.py
@@ -369,7 +369,7 @@ class LStockData():
                         last_data = kline_rows[kmin][-1]
 
                     
-                    for kline_data in kline_datas[:-1]: # [{"day":"2020-08-07 15:00:00","open":"20.390","high":"20.390","low":"20.300","close":"20.300","volume":"54500"}, ...]
+                    for kline_data in kline_datas: # [{"day":"2020-08-07 15:00:00","open":"20.390","high":"20.390","low":"20.300","close":"20.300","volume":"54500"}, ...]
                         day = int(datetime.datetime.strptime(kline_data['day'], '%Y-%m-%d %H:%M:%S').timestamp())
                         
                         if last_data is None or last_data[0] < day:
