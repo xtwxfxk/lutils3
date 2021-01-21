08b4eb2d84d56435a85c824028ff8b17ed9d10a0
left5 <zhanghuaqiang@dcorepay.com>
2021-01-21 17:23:23 +0800

    '0121'

--- lutils/stock/__init__.py
+++ lutils/stock/__init__.py
@@ -4,3 +4,4 @@ __author__ = 'xtwxfxk'
 from .lstock import lstock
 from .lstock_data import LStockData, LStockLoader, get_new_stock_code, get_all_codes, get_codes
 from .lplot import plot_stock
+from .lpytdx import LTdxHq, Category, Market
