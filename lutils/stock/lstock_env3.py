a60a9f9a70d655e6f74c5198eda0cb8ad14697ee
left5 <xtwxfxk@163.com>
2021-09-03 17:54:59 +0800

    0903

--- lutils/stock/lstock_env3.py
+++ lutils/stock/lstock_env3.py
@@ -27,7 +27,12 @@ class LStockDailyEnv(gym.Env):
 
         # self.days = []
         self.step_index = 1
-        self.df = df
+
+        self.split_days(df)
+        self.df = random.choice(self.dfs)
+        print(self.df)
+        # self.df = df
+
         self.is_eval = is_eval
         # row_index = 0
         # while row_index < df.shape[0]:
@@ -43,8 +48,9 @@ class LStockDailyEnv(gym.Env):
         # self.day = pd.concat([self.yesterday[:], self.days[self.current_day]])
 
         # self.reward_range = (0, MAX_ACCOUNT_BALANCE)
-        self.reward_range = (0, 1)
+        # self.reward_range = (0, 1)
         # self.reward_range = (0, 100)
+        self.reward_range = (-np.inf, np.inf)
 
         # Actions of the format Buy x%, Sell x%, Hold, etc.
         self.action_space = spaces.Box(low=np.array([0, 0]), high=np.array([3, 1]), dtype=np.float16)
@@ -52,40 +58,74 @@ class LStockDailyEnv(gym.Env):
         # Prices contains the OHCL values for the last five prices
         self.observation_space = spaces.Box(low=0, high=1, shape=(11, NEXT_OBSERVATION_SIZE), dtype=np.float16)
 
+    def split_days(self, df):
+
+        self.dfs = []
+        row_index = 0
+        while row_index < df.shape[0]:
+            day = df[row_index:row_index+240]
+            if len(self.dfs) < 1:
+                day = pd.concat([pd.DataFrame(np.zeros([NEXT_OBSERVATION_SIZE, day.shape[1]]), columns=day.columns), day])
+            else:
+                day = pd.concat([self.dfs[-1][-NEXT_OBSERVATION_SIZE:], day])
+            self.dfs.append(day)
+            row_index = row_index + 240
+
+        # if self.dfs[-1].shape[0] < 240:
+        #     self.dfs.pop(-1)
+
+
     def _next_observation(self):
         # Get the stock data points for the last 5 days and scale to between 0-1
         frame = np.array([
-            self.df.iloc[self.current_step: self.current_step + NEXT_OBSERVATION_SIZE]['open'].values / MAX_SHARE_PRICE,
-            self.df.iloc[self.current_step: self.current_step + NEXT_OBSERVATION_SIZE]['high'].values / MAX_SHARE_PRICE,
-            self.df.iloc[self.current_step: self.current_step + NEXT_OBSERVATION_SIZE]['low'].values / MAX_SHARE_PRICE,
-            self.df.iloc[self.current_step: self.current_step + NEXT_OBSERVATION_SIZE]['close'].values / MAX_SHARE_PRICE,
-            self.df.iloc[self.current_step: self.current_step + NEXT_OBSERVATION_SIZE]['vol'].values / MAX_NUM_SHARES,
+            # self.df.iloc[self.current_step: self.current_step + NEXT_OBSERVATION_SIZE]['open'].values / MAX_SHARE_PRICE,
+            # self.df.iloc[self.current_step: self.current_step + NEXT_OBSERVATION_SIZE]['high'].values / MAX_SHARE_PRICE,
+            # self.df.iloc[self.current_step: self.current_step + NEXT_OBSERVATION_SIZE]['low'].values / MAX_SHARE_PRICE,
+            # self.df.iloc[self.current_step: self.current_step + NEXT_OBSERVATION_SIZE]['close'].values / MAX_SHARE_PRICE,
+            # self.df.iloc[self.current_step: self.current_step + NEXT_OBSERVATION_SIZE]['vol'].values / MAX_NUM_SHARES,
 
-            self.df['macd'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].values,
-            self.df['macdh'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].values,
-            self.df['macds'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].values,
-            # # self.df['volume_delta'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].values,
-            # # self.df['open_2_d'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].values,
-            # # self.df['open_-2_r'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].values,
-            # # self.df['cr'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].values,
-            # # self.df['cr-ma1'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].fillna(0).values,
-            # # self.df['cr-ma2'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].fillna(0).values,
-            # # self.df['cr-ma3'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].fillna(0).values,
-            self.df['kdjk'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].values,
-            self.df['kdjd'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].values,
-            self.df['kdjj'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].values,
-            # self.df['open_2_sma'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].fillna(0).values,
-            # self.df['dma'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].fillna(0).values,
-            # self.df['pdi'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].fillna(0).values,
-            # self.df['mdi'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].fillna(0).values,
-            # self.df['dx'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].fillna(0).values,
-            # self.df['adx'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].fillna(0).values,
-            # self.df['adxr'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].fillna(0).values,
-            # # self.df['tema'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].values,
-            # # self.df['vr'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].fillna(0).values,
-            # # self.df['vr_6_sma'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].values,
+            # self.df['macd'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].values,
+            # self.df['macdh'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].values,
+            # self.df['macds'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].values,
+            # # # self.df['volume_delta'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].values,
+            # # # self.df['open_2_d'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].values,
+            # # # self.df['open_-2_r'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].values,
+            # # # self.df['cr'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].values,
+            # # # self.df['cr-ma1'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].fillna(0).values,
+            # # # self.df['cr-ma2'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].fillna(0).values,
+            # # # self.df['cr-ma3'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].fillna(0).values,
+            # self.df['kdjk'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].values,
+            # self.df['kdjd'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].values,
+            # self.df['kdjj'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].values,
+            # # self.df['open_2_sma'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].fillna(0).values,
+            # # self.df['dma'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].fillna(0).values,
+            # # self.df['pdi'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].fillna(0).values,
+            # # self.df['mdi'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].fillna(0).values,
+            # # self.df['dx'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].fillna(0).values,
+            # # self.df['adx'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].fillna(0).values,
+            # # self.df['adxr'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].fillna(0).values,
+            # # # self.df['tema'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].values,
+            # # # self.df['vr'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].fillna(0).values,
+            # # # self.df['vr_6_sma'][self.current_step: self.current_step + NEXT_OBSERVATION_SIZE].values,
+
+            self.df.iloc[self.current_step: self.current_step]['open'].values / MAX_SHARE_PRICE,
+            self.df.iloc[self.current_step: self.current_step]['high'].values / MAX_SHARE_PRICE,
+            self.df.iloc[self.current_step: self.current_step]['low'].values / MAX_SHARE_PRICE,
+            self.df.iloc[self.current_step: self.current_step]['close'].values / MAX_SHARE_PRICE,
+            self.df.iloc[self.current_step: self.current_step]['vol'].values / MAX_NUM_SHARES,
+
+            self.df['macd'][self.current_step - NEXT_OBSERVATION_SIZE: self.current_step].values,
+            self.df['macdh'][self.current_step - NEXT_OBSERVATION_SIZE: self.current_step].values,
+            self.df['macds'][self.current_step - NEXT_OBSERVATION_SIZE: self.current_step].values,
+            self.df['kdjk'][self.current_step - NEXT_OBSERVATION_SIZE: self.current_step].values,
+            self.df['kdjd'][self.current_step - NEXT_OBSERVATION_SIZE: self.current_step].values,
+            self.df['kdjj'][self.current_step - NEXT_OBSERVATION_SIZE: self.current_step].values,
 
         ])
+
+        print(frame)
+        print(frame.shape)
+        print(self.df['kdjj'])
 
         return frame
 
@@ -164,20 +204,26 @@ class LStockDailyEnv(gym.Env):
         #     reward = 0
         # done = self.net_worth <= INITIAL_ACCOUNT_BALANCE * .7
 
-        done = self.current_step >= (self.df.shape[0]-MAX_OPEN_POSITIONS) or self.net_worth <= INITIAL_ACCOUNT_BALANCE * .9
+        done = self.current_step >= (self.df.shape[0] - 1) or self.net_worth <= INITIAL_ACCOUNT_BALANCE * .9
 
         obs = self._next_observation()
 
         reward = 0
 
         action_type = action[0]
-        if action_type < 1: # Buy
-            reward = 1 if obs[:-1, 3].mean() >= self.df.iloc[self.current_step]['close'] else 0
-        elif action_type >= 1 and action_type < 2: # Sell
-            reward = 1 if obs[:-1, 3].mean() <= self.df.iloc[self.current_step]['close'] and shares_held > self.shares_held else 0
-        else: # Hold
-            reward = 1 if obs[:-1, 3].mean() >= self.df.iloc[self.current_step]['close'] and self.shares_held > 0 else 0
+        # if action_type < 1: # Buy
+        #     reward = 1 if obs[:-1, 3].mean() >= self.df.iloc[self.current_step]['close'] else 0
+        # elif action_type >= 1 and action_type < 2: # Sell
+        #     reward = 1 if obs[:-1, 3].mean() <= self.df.iloc[self.current_step]['close'] and shares_held > self.shares_held else 0
+        # else: # Hold
+        #     reward = 1 if obs[:-1, 3].mean() >= self.df.iloc[self.current_step]['close'] and self.shares_held > 0 else 0
         # reward = 1 if obs[:-1, 3].mean() - self.df.iloc[self.current_step]['close'] > 0 else 0
+        if action_type < 1: # Buy
+            reward = self.df.iloc[self.current_step + 1]['close'] - self.df.iloc[self.current_step]['close']
+        elif action_type >= 1 and action_type < 2: # Sell
+            reward = self.df.iloc[self.current_step]['close'] - self.df.iloc[self.current_step + 1]['close']
+        else: # Hold
+            reward = self.df.iloc[self.current_step + 1]['close'] - self.df.iloc[self.current_step]['close']
 
         return obs, reward, done, {'net_worth': self.net_worth}
 
@@ -202,7 +248,8 @@ class LStockDailyEnv(gym.Env):
         # self.day = pd.concat([self.yesterday[-MAX_OPEN_POSITIONS:], self.days[self.current_day]])
 
         # self.current_step = random.randint(0, self.df.shape[0] - NEXT_OBSERVATION_SIZE)
-        self.current_step = 0
+        # self.current_step = 0
+        self.current_step = NEXT_OBSERVATION_SIZE
 
         return self._next_observation()
 
@@ -251,7 +298,7 @@ def test2():
     eval_env = DummyVecEnv([lambda: LStockDailyEnv(df2)])
 
     # policy_kwargs = dict(net_arch=[64, 'lstm', dict(vf=[128, 128, 128], pi=[64, 64])])
-    policy_kwargs = dict(net_arch=[128, 'lstm', dict(vf=[256, 256, 256], pi=[128, 128])])
+    policy_kwargs = dict(net_arch=[128, 'lstm', dict(vf=[256, 256], pi=[256, 256])])
     
     model = A2C('MlpLstmPolicy', env, verbose=1, policy_kwargs=policy_kwargs)
     model.learn(total_timesteps=20000)
