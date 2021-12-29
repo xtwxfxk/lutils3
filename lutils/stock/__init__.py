e065ac63d3cc623b82b5ae21dd61a43a7aede080
left5 <xtwxfxk@163.com>
2021-12-29 15:17:03 +0800

    1229

--- lutils/stock/__init__.py
+++ lutils/stock/__init__.py
@@ -2,6 +2,6 @@
 __author__ = 'xtwxfxk'
 
 from .lstock import lstock
-from .lstock_data import LStockData, LStockLoader, get_new_stock_code, get_all_codes, get_codes
+# from .lstock_data import LStockData, LStockLoader, get_new_stock_code, get_all_codes, get_codes
 from .lplot import plot_stock
 from .lpytdx import LTdxHq, Category, Market
