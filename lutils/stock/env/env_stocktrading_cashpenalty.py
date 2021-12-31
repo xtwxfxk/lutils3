f614bfa3e86a0e8470d83c8b3b9c67ea89fb4d47
left5 <xtwxfxk@163.com>
2021-12-31 12:17:51 +0800

    1231

--- lutils/stock/env/env_stocktrading_cashpenalty.py
+++ lutils/stock/env/env_stocktrading_cashpenalty.py
@@ -12,7 +12,7 @@ from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
 
 matplotlib.use("Agg")
 
-tf_logger = configure('/home/l/code/python/log/run', format_strings='tensorboard')
+tf_logger = configure('/home/l/code/python/log/run', format_strings=['tensorboard'])
 logger = configure('/home/l/code/python/log')
 
 class StockTradingEnvCashpenalty(gym.Env):
